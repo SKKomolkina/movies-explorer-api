2d965a5f8663729bbfa61c60c97314c1be86cd62
Skkomolkina <skomolkina@gmail.com>
2021-10-21 17:58:16 +0300

    added CORS

--- app.js
+++ app.js
@@ -17,6 +17,13 @@ const { login, createUser } = require('./controllers/users');
 const NotFoundError = require('./utils/Errors/NotFoundError');
 const { MONGO } = require('./utils/config');
 
+const allowedCors = [
+  'localhost:3000',
+  'http://localhost:3000',
+  'http://movies-skomolkina.nomoredomains.monster',
+  'https://movies-skomolkina.nomoredomains.monster',
+];
+
 const { PORT = 3000 } = process.env;
 
 const app = express();
@@ -26,6 +33,26 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 
 mongoose.connect(MONGO);
+
+app.use((req, res, next) => {
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
+  const { origin } = req.headers;
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.status(200).send();
+  }
+
+  return next();
+});
 
 app.use(requestLogger);
 
