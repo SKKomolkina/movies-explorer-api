ef2a382b79aa82ad237ab0c9c3f79f22d5e11db6
Skkomolkina <skomolkina@gmail.com>
2021-11-18 15:57:16 +0300

    del: limiter

--- app.js
+++ app.js
@@ -6,11 +6,6 @@ const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const { errors } = require('celebrate');
 
-const rateLimit = require('express-rate-limit');
-const rateLimiter = require('./utils/rateLimiter');
-
-const limiter = rateLimit(rateLimiter);
-
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const error = require('./middlewares/error');
 
@@ -33,7 +28,6 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
-app.use(limiter);
 
 mongoose.connect(MONGO);
 
